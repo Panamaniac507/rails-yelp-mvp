8cda08af36e0edb6c6fb2b82ab8f72caa0d84b9b
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-04 21:40:54 +0900

    models created, controllers created, validations created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning the database"
+
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name  ,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Faker::Restaurant.type,
+  )
+  puts "Restaurants with id #{restaurant.id} has been created"
+end
+
+puts "Finished"
