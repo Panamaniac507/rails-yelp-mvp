dc852d990e7faff5a640e3264ef92e0df072c36e
Joseph Herbert <herbert.jr80@gmail.com>
2023-07-05 18:08:45 +0900

    updated review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, presence: true
-  # validates :rating, inclusion: { in: [(0..5)], allow_nil: false }
+  validates :rating, length: { in: 0..5 }
   validates :rating, numericality: { only_integer: true }
 end
